d135fdf32571aa1cd04ac4e4bb05203dcbc8f1e5
neoteristis <anjou.raphael@gmail.com>
2022-10-11 09:35:08 +0200

    feat: add conversion between km and mi for the user input as well as the user display

--- src/fr/epu/bicycle/Main.java
+++ src/fr/epu/bicycle/Main.java
@@ -3,26 +3,42 @@ package fr.epu.bicycle;
 import fr.epu.bicycle.EBike;
 
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+    public static double kmToMiles(double km) {
+        return km * 0.621371;
+    }
+
+    public static double milesToKm(double miles) {
+        return miles * 1.609344;
+    }
+
     public static void main(String[] args) {
         EBike e1= new EBike();
         final String STOP = "s";
         String stop = "f";
         Scanner keyboard = new Scanner(System.in);
-        keyboard.useLocale(Locale.US);//To read the doubles with . and not ,
+        keyboard.useLocale(Locale.US); // To read the doubles with . and not ,
 
         while (! STOP.equals(stop)) {
             System.out.print("\t What distance did you travel in km or miles ? ");
-            if (keyboard.hasNextDouble()) {
-                double newDistance = keyboard.nextDouble();//reading a double
-                keyboard.nextLine();
+            if (keyboard.hasNextLine()) {
+                String input = keyboard.nextLine();// reading a double
+
+                String value = input.split(" ")[0];
+                String unit = input.split(" ")[1];
+
+                double newDistance;
+                if (Objects.equals(unit, "mi")) {
+                    newDistance = milesToKm(Double.parseDouble(value));
+                } else {
+                    newDistance = Double.parseDouble(value);
+                }
+
                 e1.addKm(newDistance);
-                System.out.println("\t Your bike has therefore travelled " + e1.getKm() + " km");
-            }
-            else {
-                System.out.println("\t a number is expected for example  1 or 1.5 " );
+                System.out.println("\t Your bike has therefore travelled " + e1.getKm() + " km" + " or " + kmToMiles(e1.getKm()) + "mi");
             }
             System.out.print("Do you want to continue or stop ("+ STOP + ") ? ");
             stop = keyboard.nextLine();
