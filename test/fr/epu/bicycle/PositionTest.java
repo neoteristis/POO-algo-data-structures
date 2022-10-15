f995c08b17298361fa00dd7eb613576684cffe18
neoteristis <anjou.raphael@gmail.com>
2022-10-15 12:44:01 +0200

    test(Position)

--- test/fr/epu/bicycle/PositionTest.java
+++ test/fr/epu/bicycle/PositionTest.java
@@ -1,5 +1,6 @@
 package fr.epu.bicycle;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -7,42 +8,77 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Position Test")
 class PositionTest {
+    private static Position p1;
+    private static Position p2;
+    private static Position p3;
+    private static Position p4;
 
-    private static final Position p1 = new Position(3,4);
-    private static final Position p2 = new Position(7,1);
+    @BeforeEach
+    void beforeEach() {
+        p1 = new Position(3, 4);
+        p2 = new Position(7, 1);
 
-    private static final Position p3 = new Position(3,4.002);
-    private static final Position p4 = new Position(2.999,4.001);
-
-    @Nested
-    @DisplayName("Test the Position object")
-    class positionInitializationTest {
-
+        p3 = new Position(3, 4.002);
+        p4 = new Position(2.999, 4.001);
     }
 
     @Nested
-    @DisplayName("Test the equivalent method")
-    class equivalentMethodTest {
-        @Test
-        @DisplayName("Test that two near enough positions are equivalent")
-        void nearEnoughPositionsAreEquivalent(){
-            assertFalse(p1.isEquivalent(p2));
-            assertTrue(p3.isEquivalent(p4));
+    @DisplayName("constructor method")
+    class ConstructorMethodTest {
+        @Nested
+        @DisplayName("without parameters")
+        class ConstructorWithoutParametersTest{
+            @Test
+            @DisplayName("Test that the coordinates are the default one")
+            void constructor_whenInitializedWithoutParameters_defaultCoordinates () {
+                Position p0 = new Position();
+                assertEquals(0, p0.getX());
+                assertEquals(0, p0.getY());
+            }
+        }
+
+        @Nested
+        @DisplayName("with parameters")
+        class ConstructorWithParametersTest {
+            @Test
+            @DisplayName("Test that the coordinates equals the one entered as parameters as numbers")
+            void constructor_whenInitializedWithTwoNumbers_coordinatesAreCorrect() {
+                // With integers
+                assertEquals(3, p1.getX());
+                assertEquals(4, p1.getY());
+
+                // With double
+                assertEquals(2.999, p4.getX());
+                assertEquals(4.001, p4.getY());
+            }
         }
     }
 
     @Nested
-    @DisplayName("Test the distance method")
-    class distanceMethodTest {
+    @DisplayName("equivalent method")
+    class EquivalentMethodTest {
+        @Test
+        @DisplayName("Test that two near enough positions are equivalent")
+        void equivalent_whenTwoPositionsAreNearEnough_true(){
+            // Near enough positions
+            assertTrue(p3.isEquivalent(p4));
+            // Not near enough positions
+            assertFalse(p1.isEquivalent(p2));
+        }
+    }
+
+    @Nested
+    @DisplayName("distance method")
+    class DistanceMethodTest {
         @Test
         @DisplayName("Test that the distance is commutative")
-        void distanceIsCommutative() {
+        void distance_whenPositionsAreSwitched_isTheSame() {
             assertEquals(p1.distance(p2), p2.distance(p1));
         }
 
         @Test
         @DisplayName("Test that the distance is calculated correctly")
-        void distanceIsCalculatedCorrectly() {
+        void distance_whenGivenTwoPositions_isCalculatedCorrectly() {
             assertEquals(5, p2.distance(p1), 0.001);
             assertNotEquals(1, p3.distance(p4), 0.001);
         }
